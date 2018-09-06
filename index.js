92d249c440bc2c0f7d22458cf12254db80b3d233
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-06 01:07:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 var theFunk = function funkyFunction() {
-  return 
+  return function funkyFunction() {
+}
 }
 
